a587a2e3d031cdfd833b2df0610afd7e43cb6aa9
Md Jakaria Nur <104752446+mdjakarianur@users.noreply.github.com>
2024-11-26 17:33:13 +0600

    Create LinkedList Basic Code.java

---
+++ LinkedList Basic Code.java
@@ -0,0 +1,13 @@
+
+import java.util.LinkedList;
+
+public class Main { 
+  public static void main(String[] args) { 
+    LinkedList<String> name = new LinkedList<String>();
+    name.add("Jakaria");
+    name.add("Ansari");
+    name.add("Hafiz");
+    name.add("Reduan");
+    System.out.println(name);
+  } 
+}
